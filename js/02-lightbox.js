4ae6e36f1373ccfe6f0dec0e9e7b9f5e729aa79e
Ribich1 <ribich@ukr.net>
2023-05-08 16:07:48 +0200

    Task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryUl = document.querySelector(".gallery");
+
+
+const markupItem = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item"><a class="gallery__link" href=${original}> <img 
+   class="gallery__image" src=${preview} alt="${ description }" /> </a></li>`
+).join('');
+
+galleryUl.insertAdjacentHTML('afterbegin', markupItem);
+
+ let gallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250
+    });
+
